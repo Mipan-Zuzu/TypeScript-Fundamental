9a5642952231bb54a981afd2180a93d9e9a781ba
Mipan-Zuzu <kadekisaka00@gmail.com>
2026-03-16 08:08:10 +0800

    feat: type data primitif

---
+++ test/tipe-data.test.ts
@@ -0,0 +1,9 @@
+describe('data type', () => {
+    it("should must declared", () => {
+        const nama: string = "mipam"
+        const age:number = 16
+        const maried:boolean = false
+        
+        console.info(nama, age, maried)
+    })
+})
