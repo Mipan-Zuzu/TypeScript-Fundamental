20c572e0e4f78fd1ae6d520e7b7ee39a864e2381
Mipan-Zuzu <kadekisaka00@gmail.com>
2026-03-16 12:51:42 +0800

    feat : Object type

--- src/type-alias.ts
+++ src/type-alias.ts
@@ -12,3 +12,22 @@ export type Barang = {
     categories: Categories
     isAvailable: boolean
 }
+
+export type rand1 = string | number
+export type rands1 = string | boolean
+
+export type rand2 = {
+    id: rand1
+    name: string
+}
+
+export type rand3 = {
+    id: rand1
+    name: string
+    isAvailable: rands1
+}
+
+export type rand4 = {
+    information: rand2 | rand3
+    data: rand1 | rand2
+}
---
+++ test/objext.test.ts
@@ -0,0 +1,14 @@
+import type { ID } from "../src/type-alias"
+
+describe('Object', () => {
+  it("should object alias type", () => {
+
+    const user: {id: ID, name: string} = {
+        id: "ZXgY6RpLn",
+        name: "mipan"
+    }
+
+    console.info(user)
+
+  })
+})
