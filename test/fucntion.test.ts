cdb516ed937b02625b7548ffb1d03dba137280b2
Mipan-Zuzu <kadekisaka00@gmail.com>
2026-03-19 20:30:24 +0800

    feat: Optional Function

--- test/fucntion.test.ts
+++ test/fucntion.test.ts
@@ -1,7 +1,8 @@
+
 describe('function', () => {
   it("should use function type", () => {
 
-    const sayHello = (name: string): string => {
+    const sayHello = (name: string ): string => {
         return `halo ${name}`
     }
 
@@ -10,6 +11,42 @@ describe('function', () => {
     }
 
     expect(sayHello("mipan")).toBe("halo mipan")
+
+  })
+
+  it("should support default value", () => {
+
+    const sayHello = (name: string = "user"): string => {
+        return `helo ${name}`
+    }
+
+    expect(sayHello("suki")).toBe("helo suki")
+    expect(sayHello()).toBe("helo user")
+
+  })
+
+  it("should support rest parameter", () => {
+
+    const sum = (...values: number[]) : number => {
+        let result: number = 0
+        for(const value of values) {
+            result += value
+        }
+        return result
+    }
+
+    expect(sum(1,3,3,3,4)).toBe(14)
+
+  })
+
+  it("sould support optional parameter", () => {
+    
+    const sayHello = (fristname:string , lastName?: string):string => {
+        return lastName? `helo ${fristname} ${lastName}` : `helo ${fristname}`
+    }
+
+    expect(sayHello("mipan")).toBe("helo mipan")
+    expect(sayHello("mipan", "suki")).toBe("helo mipan suki")
 
   })
 })
